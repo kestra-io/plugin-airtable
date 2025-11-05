ccabce606c9cf00f66139f17763818e1180844f6
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-05 17:57:29 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/airtable/records/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Airtable Record",
+    description = "This sub-group of plugins contains tasks to interact with Airtable Records.",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.airtable.records;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
